598ae700416e948a00a91d3bae712c7a17e3f797
sahand <sahandbabali@gmail.com>
2020-01-02 16:01:13 +0330

    Updates

---
+++ precache-manifest.f1833f7493a1ee7269eda445dfab8501.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed7a39e0790a834471c90cc62c8b2c43",
+    "url": "/ytgard/index.html"
+  },
+  {
+    "revision": "b7edebdbd53f7ab2ef96",
+    "url": "/ytgard/static/css/main.0c5d2e62.chunk.css"
+  },
+  {
+    "revision": "607cdf01953b081d4c9a",
+    "url": "/ytgard/static/js/2.688337c7.chunk.js"
+  },
+  {
+    "revision": "b7edebdbd53f7ab2ef96",
+    "url": "/ytgard/static/js/main.3ae1f90a.chunk.js"
+  },
+  {
+    "revision": "dd2eb17dbd2ba70d9812",
+    "url": "/ytgard/static/js/runtime-main.856f7c60.js"
+  },
+  {
+    "revision": "0b5055ac357359f8c23320ea3dc0f78b",
+    "url": "/ytgard/static/media/IRANSansWeb.0b5055ac.woff2"
+  },
+  {
+    "revision": "66b5cb30e2efe84dab915617acd39041",
+    "url": "/ytgard/static/media/IRANSansWeb.66b5cb30.woff"
+  },
+  {
+    "revision": "c26a9eb202b3ef6455281f0335cfffe9",
+    "url": "/ytgard/static/media/IRANSansWeb.c26a9eb2.ttf"
+  },
+  {
+    "revision": "d3931868176480f01fd4524cbd48b667",
+    "url": "/ytgard/static/media/IRANSansWeb.d3931868.eot"
+  },
+  {
+    "revision": "1becfaba43eef43af02099d64f0590a6",
+    "url": "/ytgard/static/media/IRANSansWeb_Black.1becfaba.ttf"
+  },
+  {
+    "revision": "c0f5dbbde9104d1dc813b68f50457e2c",
+    "url": "/ytgard/static/media/IRANSansWeb_Black.c0f5dbbd.eot"
+  },
+  {
+    "revision": "dfb26a2f06ef268a82147b6ea001bcf9",
+    "url": "/ytgard/static/media/IRANSansWeb_Black.dfb26a2f.woff"
+  },
+  {
+    "revision": "f477dd9634c960c5ce215aa435e32b4c",
+    "url": "/ytgard/static/media/IRANSansWeb_Black.f477dd96.woff2"
+  },
+  {
+    "revision": "43a0ecf3c7f2af819b192d1284f95ed9",
+    "url": "/ytgard/static/media/IRANSansWeb_Bold.43a0ecf3.woff2"
+  },
+  {
+    "revision": "e4ed975d7eb3ca8da589f27e55ef7f20",
+    "url": "/ytgard/static/media/IRANSansWeb_Bold.e4ed975d.woff"
+  },
+  {
+    "revision": "ebec177a9eccde9d5a4fb510ff1d1d8e",
+    "url": "/ytgard/static/media/IRANSansWeb_Bold.ebec177a.ttf"
+  },
+  {
+    "revision": "f2cb5d5906f5abc3c9f59e561b007ded",
+    "url": "/ytgard/static/media/IRANSansWeb_Bold.f2cb5d59.eot"
+  },
+  {
+    "revision": "28bc7e37dc1bba427c24941b19a11e4a",
+    "url": "/ytgard/static/media/IRANSansWeb_Light.28bc7e37.eot"
+  },
+  {
+    "revision": "63f8b4b6f55a927231311a9e83cb4fda",
+    "url": "/ytgard/static/media/IRANSansWeb_Light.63f8b4b6.woff"
+  },
+  {
+    "revision": "e8ff3f7167961a35e98db2fd15bb37ca",
+    "url": "/ytgard/static/media/IRANSansWeb_Light.e8ff3f71.ttf"
+  },
+  {
+    "revision": "f7e4be98d20eb763b867143da5207b90",
+    "url": "/ytgard/static/media/IRANSansWeb_Light.f7e4be98.woff2"
+  },
+  {
+    "revision": "0c40b1a0ee0e7fedeb8a4ac6c58e270b",
+    "url": "/ytgard/static/media/IRANSansWeb_Medium.0c40b1a0.woff"
+  },
+  {
+    "revision": "1da39fcf35d32c87ce9b3bdcfd2b90b2",
+    "url": "/ytgard/static/media/IRANSansWeb_Medium.1da39fcf.ttf"
+  },
+  {
+    "revision": "3d468556450b88fba2ef7ac695a166d3",
+    "url": "/ytgard/static/media/IRANSansWeb_Medium.3d468556.eot"
+  },
+  {
+    "revision": "9c66b762719d40d1f18e678a1405459a",
+    "url": "/ytgard/static/media/IRANSansWeb_Medium.9c66b762.woff2"
+  },
+  {
+    "revision": "068169d2ab5f8a5dd1beae8b0149217d",
+    "url": "/ytgard/static/media/IRANSansWeb_UltraLight.068169d2.woff2"
+  },
+  {
+    "revision": "3a70f4b4dcc84bcb84c3749ec235338d",
+    "url": "/ytgard/static/media/IRANSansWeb_UltraLight.3a70f4b4.eot"
+  },
+  {
+    "revision": "51b9c1ad55d006c89ec0639de7892278",
+    "url": "/ytgard/static/media/IRANSansWeb_UltraLight.51b9c1ad.ttf"
+  },
+  {
+    "revision": "bb7ec39fffa2e3ce96a5f52edd9e13c4",
+    "url": "/ytgard/static/media/IRANSansWeb_UltraLight.bb7ec39f.woff"
+  }
+]);
